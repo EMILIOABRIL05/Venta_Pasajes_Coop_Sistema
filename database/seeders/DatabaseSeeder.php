8274f94e28b2a132b0d70de5c51e485108eee565
Kevin <keander300@gmail.com>
2026-04-28 14:15:15 -0500

    feat: registra seeders operativos en DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            ParadaSeeder::class,
+            RutaSeeder::class,
+            FrecuenciaSeeder::class,
         ]);
     }
 }
