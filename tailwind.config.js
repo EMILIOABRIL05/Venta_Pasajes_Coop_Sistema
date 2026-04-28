556753b24438c8f4c910b4fabfae1f97bb2079a8
Emilio <alexanderabril552@gmail.com>
2026-04-27 22:25:13 -0500

    feat: agregar colores de marca Cooperativa Ambato en Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,17 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'ambato-azul': '#003366',
+        'ambato-rojo': '#CC0000',
+      },
     },
-
-    plugins: [forms],
-};
+  },
+  plugins: [],
+}
