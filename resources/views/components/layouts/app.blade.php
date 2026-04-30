3d1ee2e56e6d6c7522443e1a183d091b4505951c
Manuel Cusme <cusmeman06@gmail.com>
2026-04-30 01:24:17 -0500

    Revert "refactor: consolidar layout en components/layouts/app.blade.php"
    This reverts commit 59b163e41a3777d0f462a8b296ac678c23376c2c.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -16,29 +16,23 @@
     </head>
     <body class="font-sans antialiased bg-gray-100 text-gray-800">
         <div class="min-h-screen bg-gray-100">
-            @auth
-                @include('components.sidebar-navigation')
-            @endauth
+            <x-ui.navbar />
 
-            <div class="flex flex-col {{ auth()->check() ? 'sm:ml-64' : '' }}">
-                <x-ui.navbar />
-
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <x-ui.alert />
-                </div>
-
-                @isset($header)
-                    <header class="bg-gray-100 border-b border-[#003366]/10">
-                        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                            {{ $header }}
-                        </div>
-                    </header>
-                @endisset
-
-                <main class="py-6">
-                    {{ $slot }}
-                </main>
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <x-ui.alert />
             </div>
+
+            @isset($header)
+                <header class="bg-gray-100 border-b border-[#003366]/10">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endisset
+
+            <main class="py-6">
+                {{ $slot }}
+            </main>
         </div>
     </body>
 </html>
