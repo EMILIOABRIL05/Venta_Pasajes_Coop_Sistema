4e51af5b1e30f8b6cb1924f234f70ea920503464
zamukay <133037731+zamukay@users.noreply.github.com>
2026-05-07 23:28:32 -0500

    fix: redireccion por roles en login y navbar publico web

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,11 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        if ($request->user()->hasAnyRole(['admin', 'oficinista'])) {
+            return redirect()->intended(route('dashboard', absolute: false));
+        }
+
+        return redirect()->intended('/');
     }
 
     /**
