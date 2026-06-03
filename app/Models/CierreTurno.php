4ebac965e54084d19b9260c9503fbfddef46adf6
Manuel Cusme <cusmeman06@gmail.com>
2026-06-02 23:11:37 -0500

    feat: habilita softdeletes en modelos de auditoria

--- app/Models/CierreTurno.php
+++ app/Models/CierreTurno.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CierreTurno extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'cierres_turno';
 
     protected $fillable = [
